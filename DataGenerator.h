fcc403c60d8a5dfbee9387347dd3bfa293b742d4
vhtanloc <vhtloc20@clc.fitus.edu.vn>
2025-03-18 21:03:17 +0700

    Add files via upload

---
+++ DataGenerator.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <time.h>
+#pragma once
+
+using namespace std;
+
+template <class T>
+void HoanVi(T& a, T& b);
+void GenerateRandomData(int a[], int n);
+void GenerateSortedData(int a[], int n);
+void GenerateReverseData(int a[], int n);
+void GenerateNearlySortedData(int a[], int n);
+void GenerateData(int a[], int n, int dataType);
